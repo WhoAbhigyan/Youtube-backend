ce1dbb236c0d958f5b1c9cafef9ce02cb3828437
WhoAbhigyan <abhigyanraj18@gmail.com>
2026-08-07 15:47:42 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken=function(){
             _id:this._id,//from database
             email:this.email,
             username:this.username,
-            fullName:this.fullname,
+            fullName:this.fullName,
         },
         process.env.ACCESS_TOKEN_SECERET,
         {
