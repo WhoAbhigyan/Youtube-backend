e0f4c8588689c5aaf1b6a7443d8a97b70bf84ffb
WhoAbhigyan <abhigyanraj18@gmail.com>
2026-08-11 15:09:19 +0530

    bug fixex in regsiter controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,8 +23,7 @@ const registerUser=asyncHandler(async(req,res) => {
     //  }
     if(
         [fullName,username,email,password].some((field)=>{
-            return 
-            field?.trim()===""
+            return field?.trim()==="";
         })
     ){
         throw new ApiError(400,"All fields are required")
@@ -32,21 +31,22 @@ const registerUser=asyncHandler(async(req,res) => {
     if(!email.includes("@") || email.endsWith("@") || email.startsWith("@")){
         throw new ApiError(400,"valid email is required")
     }
-    const existedUser =User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username},{email}]
     })
     if(existedUser){
+        //409 means Conflict
         throw new ApiError(409,"User with email or username already exists")
     }
-    const avatarLocalPath=req.files?.avatar[0]?.path
-    const coverImageLoaclPath=req.files?.coverImage[0]?.path
+    const avatarLocalPath=req.files?.avatar?.[0]?.path
+    const coverImageLocalPath=req.files?.coverImage?.[0]?.path
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
 
     const avatar=await uploadOnCloudinary(avatarLocalPath)
-    const coverImage=await uploadOnCloudinary(coverImageLoaclPath)
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
         throw new ApiError(400,"Avatar is required")
